8be4ae6046e5b545a90fab699871763ecf96a86b
anhha <hangocanh2303@gmail.com>
2017-09-26 15:49:12 +0700

    add current location

--- app/src/main/java/com/bkstek/stour/MapsActivity.java
+++ app/src/main/java/com/bkstek/stour/MapsActivity.java
@@ -1,359 +1,130 @@
 package com.bkstek.stour;
 
-import android.content.Context;
-import android.graphics.Color;
-import android.location.Address;
-import android.location.Geocoder;
-import android.os.AsyncTask;
-import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
-import android.util.Log;
-import android.view.View;
-import android.view.inputmethod.InputMethodManager;
-import android.widget.EditText;
-import android.widget.Toast;
-
-import com.bkstek.stour.mapdigital.TileProviderFactory;
-import com.bkstek.stour.util.DirectionsJSONParser;
-import com.google.android.gms.maps.CameraUpdateFactory;
+import com.bkstek.stour.helper.PermissionUtils;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.GoogleMap.OnMyLocationButtonClickListener;
+import com.google.android.gms.maps.GoogleMap.OnMyLocationClickListener;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.BitmapDescriptor;
-import com.google.android.gms.maps.model.BitmapDescriptorFactory;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.LatLngBounds;
-import com.google.android.gms.maps.model.MarkerOptions;
-import com.google.android.gms.maps.model.PolylineOptions;
-import com.google.android.gms.maps.model.TileOverlayOptions;
-import com.google.android.gms.maps.model.TileProvider;
 
-import org.json.JSONObject;
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Locale;
+/**
+ * This demo shows how GMS Location can be used to check for changes to the users location.  The
+ * "My Location" button uses GMS Location to set the blue dot representing the users location.
+ * Permission for {@link android.Manifest.permission#ACCESS_FINE_LOCATION} is requested at run
+ * time. If the permission has not been granted, the Activity is finished with an error message.
+ */
+public class MapsActivity extends AppCompatActivity
+        implements
+        OnMyLocationButtonClickListener,
+        OnMyLocationClickListener,
+        OnMapReadyCallback,
+        ActivityCompat.OnRequestPermissionsResultCallback {
 
-import at.markushi.ui.CircleButton;
+    /**
+     * Request code for location permission request.
+     *
+     * @see #onRequestPermissionsResult(int, String[], int[])
+     */
+    private static final int LOCATION_PERMISSION_REQUEST_CODE = 1001;
 
-import static com.bkstek.stour.util.CommonDefine.GEOSERVER_FORMAT;
-import static com.bkstek.stour.util.CommonDefine.GOOGLEMAP_DIRECTION;
-import static com.bkstek.stour.util.CommonDefine.WMS_FORMAT_ROUTE_STRING;
-
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+    /**
+     * Flag indicating whether a requested permission has been denied after returning in
+     * {@link #onRequestPermissionsResult(int, String[], int[])}.
+     */
+    private boolean mPermissionDenied = false;
 
     private GoogleMap mMap;
-    EditText edFrom, edTo;
-    Context context;
-    //Button btnSearch;
-    CircleButton btnSearch;
-    BitmapDescriptor bitmapDescriptorTo;
-    BitmapDescriptor bitmapDescriptorFrom;
-    Address locationFrom = null;
-    Address locationTo = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
-        edFrom = (EditText) findViewById(R.id.edFrom);
-        edTo = (EditText) findViewById(R.id.edTo);
-        btnSearch = (CircleButton) findViewById(R.id.btnSearch);
-        context = MapsActivity.this;
 
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
+        SupportMapFragment mapFragment =
+                (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
-        //addMapEvents();
-
-        bitmapDescriptorFrom
-                = BitmapDescriptorFactory.defaultMarker(
-                BitmapDescriptorFactory.HUE_RED);
-
-        bitmapDescriptorTo
-                = BitmapDescriptorFactory.defaultMarker(
-                BitmapDescriptorFactory.HUE_AZURE);
-
-        btnSearch.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                hideSoftKeyboard();
-                ClearMap();
-                setUpMap();
-                getAddress();
-            }
-        });
-
-
     }
 
-    private void ClearMap() {
-        if (mMap != null)
-            mMap.clear();
+    @Override
+    public void onMapReady(GoogleMap map) {
+        mMap = map;
+
+        mMap.setOnMyLocationButtonClickListener(this);
+        mMap.setOnMyLocationClickListener(this);
+        enableMyLocation();
     }
-
-    private void getAddress() {
-        String addressFrom = edFrom.getText().toString();
-        String addressTo = edTo.getText().toString();
-        if (addressFrom.equals("")) {
-            Toast.makeText(context, "Bạn chưa nhập địa chỉ điểm đi", Toast.LENGTH_LONG).show();
-        } else if (addressTo.equals("")) {
-            Toast.makeText(context, "Bạn chưa nhập địa chỉ điểm đến", Toast.LENGTH_LONG).show();
-        } else {
-            getLocationFromAddress(addressFrom, addressTo);
-            RoutingInMap(locationFrom, locationTo);
-        }
-    }
-
-    private void setUpMap() {
-        TileProvider tileProvider = TileProviderFactory.getTileProvider(GEOSERVER_FORMAT);
-        mMap.addTileOverlay(new TileOverlayOptions().tileProvider(tileProvider));
-    }
-
-
-    // dinh tuyen tren ban do so hoa thong qua kinh do vi do diem dau va diem cuoi
-    private void RoutingInMap(Address locationF, Address locationT) {
-        String url = WMS_FORMAT_ROUTE_STRING + "=x1:" + locationF.getLongitude() + ";y1:" + locationF.getLatitude() + ";x2:" + locationT.getLongitude()
-                + ";y2:" + locationT.getLatitude() + "&SERVICE=WMS&VERSION=1.1.1" +
-                "&REQUEST=GetMap&STYLES=&SRS=EPSG:3857" +
-                "&BBOX=%f,%f,%f,%f" +
-                "&WIDTH=256&HEIGHT=256";
-
-        Log.d("duytho", url);
-
-        TileProvider tileProvider_route = TileProviderFactory.getTileProvider(url);
-        mMap.addTileOverlay(new TileOverlayOptions().tileProvider(tileProvider_route));
-    }
-
-
-    public void getLocationFromAddress(String strAddressFrom, String strAddressTo) {
-
-        Geocoder coder = new Geocoder(this, Locale.US);
-        List<Address> addressFrom;
-        List<Address> addressTo;
-        try {
-            addressFrom = coder.getFromLocationName(strAddressFrom, 5);
-            addressTo = coder.getFromLocationName(strAddressTo, 5);
-            try {
-                locationFrom = addressFrom.get(0);
-                locationTo = addressTo.get(0);
-            } catch (Exception e) {
-                Toast.makeText(context, "Địa chỉ bạn nhập không tồn tại. Vui lòng nhập lại", Toast.LENGTH_LONG).show();
-            }
-            
-
-            LatLng from = new LatLng(locationFrom.getLatitude(), locationFrom.getLongitude());
-
-            mMap.addMarker(new MarkerOptions().position(from).icon(bitmapDescriptorFrom).title(strAddressFrom)).showInfoWindow();
-
-
-            LatLng to = new LatLng(locationTo.getLatitude(), locationTo.getLongitude());
-            mMap.addMarker(new MarkerOptions().position(to).icon(bitmapDescriptorTo).title(strAddressTo)).showInfoWindow();
-
-            LatLngBounds.Builder builder = new LatLngBounds.Builder();
-            builder.include(from);
-            builder.include(to);
-            LatLngBounds bound = builder.build();
-            mMap.animateCamera(CameraUpdateFactory.newLatLngBounds(bound, 25), 1000, null);
-
-            // Getting URL to the Google Directions API
-            String url = getDirectionUrl(from, to);
-
-            DownloadTask downloadTask = new DownloadTask();
-
-            // Start downloading json data from Google Directions API
-            downloadTask.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR, url);
-
-        } catch (IOException e) {
-            // Toast.makeText(context, "Địa chỉ bạn nhập không tồn tại. Vui lòng nhập lại", Toast.LENGTH_LONG).show();
-        }
-    }
-
-
-    //region dinh tuyen bang google map
-    //@author journaldev
-
-    private String getDirectionUrl(LatLng origin, LatLng dest) {
-        // Origin of route
-        String str_origin = "origin=" + origin.latitude + "," + origin.longitude;
-
-        // Destination of route
-        String str_dest = "destination=" + dest.latitude + "," + dest.longitude;
-
-        // Sensor enabled
-        String sensor = "sensor=false";
-        String mode = "mode=driving";
-        // Building the parameters to the web service
-        String parameters = str_origin + "&" + str_dest + "&" + sensor + "&" + mode;
-        // Output format
-        String output = "json";
-        // Building the url to the web service
-        String url = GOOGLEMAP_DIRECTION + output + "?" + parameters;
-
-        return url;
-    }
-
-    private String downloadUrl(String strUrl) throws IOException {
-        String data = "";
-        InputStream iStream = null;
-        HttpURLConnection urlConnection = null;
-        try {
-            URL url = new URL(strUrl);
-
-            urlConnection = (HttpURLConnection) url.openConnection();
-
-            urlConnection.connect();
-
-            iStream = urlConnection.getInputStream();
-
-            BufferedReader br = new BufferedReader(new InputStreamReader(iStream));
-
-            StringBuffer sb = new StringBuffer();
-
-            String line = "";
-            while ((line = br.readLine()) != null) {
-                sb.append(line);
-            }
-
-            data = sb.toString();
-
-            br.close();
-
-        } catch (Exception e) {
-            Log.d("Exception", e.toString());
-        } finally {
-            iStream.close();
-            urlConnection.disconnect();
-        }
-        return data;
-    }
-
-
-    private class DownloadTask extends AsyncTask<String, Void, String> {
-
-        @Override
-        protected String doInBackground(String... url) {
-
-            String data = "";
-
-            try {
-                data = downloadUrl(url[0]);
-            } catch (Exception e) {
-                Log.d("Background Task", e.toString());
-            }
-            return data;
-        }
-
-        @Override
-        protected void onPostExecute(String result) {
-            super.onPostExecute(result);
-
-            ParserTask parserTask = new ParserTask();
-
-            parserTask.executeOnExecutor(THREAD_POOL_EXECUTOR, result);
-
-        }
-    }
-
-
-    private class ParserTask extends AsyncTask<String, Integer, List<List<HashMap<String, String>>>> {
-
-        // Parsing the data in non-ui thread
-        @Override
-        protected List<List<HashMap<String, String>>> doInBackground(String... jsonData) {
-
-            JSONObject jObject;
-            List<List<HashMap<String, String>>> routes = null;
-
-            try {
-                jObject = new JSONObject(jsonData[0]);
-                DirectionsJSONParser parser = new DirectionsJSONParser();
-
-                routes = parser.parse(jObject);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-            return routes;
-        }
-
-        @Override
-        protected void onPostExecute(List<List<HashMap<String, String>>> result) {
-            ArrayList points = null;
-            PolylineOptions lineOptions = null;
-            MarkerOptions markerOptions = new MarkerOptions();
-
-            for (int i = 0; i < result.size(); i++) {
-                points = new ArrayList();
-                lineOptions = new PolylineOptions();
-
-                List<HashMap<String, String>> path = result.get(i);
-
-                for (int j = 0; j < path.size(); j++) {
-                    HashMap<String, String> point = path.get(j);
-
-                    double lat = Double.parseDouble(point.get("lat"));
-                    double lng = Double.parseDouble(point.get("lng"));
-                    LatLng position = new LatLng(lat, lng);
-
-                    points.add(position);
-                }
-
-                lineOptions.addAll(points);
-                lineOptions.width(12);
-                lineOptions.color(Color.RED);
-                lineOptions.geodesic(true);
-
-            }
-
-            // Drawing polyline in the Google Map for the i-th route
-            mMap.addPolyline(lineOptions);
-        }
-    }
-
-
-    //endregion
-
 
     /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
+     * Enables the My Location layer if the fine location permission has been granted.
      */
+    private void enableMyLocation() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            // Permission to access the location is missing.
+            PermissionUtils.requestPermission(this, LOCATION_PERMISSION_REQUEST_CODE,
+                    Manifest.permission.ACCESS_FINE_LOCATION, true);
+        } else if (mMap != null) {
+            // Access to the location has been granted to the app.
+            mMap.setMyLocationEnabled(true);
+        }
+    }
+
     @Override
-    public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
+    public boolean onMyLocationButtonClick() {
+        Toast.makeText(this, "MyLocation button clicked", Toast.LENGTH_SHORT).show();
+        // Return false so that we don't consume the event and the default behavior still occurs
+        // (the camera animates to the user's current position).
+        return false;
+    }
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(21.004508, 105.844160);
-//        mMap.setMinZoomPreference(18);
-//        mMap.setMaxZoomPreference(21);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Thư viện Tạ Quang Bửu")).showInfoWindow();
-        //  mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(21.004508, 105.844160), 16.0f));
+    @Override
+    public void onMyLocationClick(@NonNull Location location) {
+        Toast.makeText(this, "Current location:\n" + location, Toast.LENGTH_LONG).show();
+    }
 
-        if (mMap != null) {
-            setUpMap();
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        if (requestCode != LOCATION_PERMISSION_REQUEST_CODE) {
+            return;
+        }
+
+        if (PermissionUtils.isPermissionGranted(permissions, grantResults,
+                Manifest.permission.ACCESS_FINE_LOCATION)) {
+            // Enable the my location layer if the permission has been granted.
+            enableMyLocation();
+        } else {
+            // Display the missing permission error dialog when the fragments resume.
+            mPermissionDenied = true;
         }
     }
 
-    public void hideSoftKeyboard() {
-        if (getCurrentFocus() != null) {
-            InputMethodManager inputMethodManager = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
-            inputMethodManager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), 0);
+    @Override
+    protected void onResumeFragments() {
+        super.onResumeFragments();
+        if (mPermissionDenied) {
+            // Permission was not granted, display error dialog.
+            showMissingPermissionError();
+            mPermissionDenied = false;
         }
     }
 
+    /**
+     * Displays a dialog with error message explaining that the location permission is missing.
+     */
+    private void showMissingPermissionError() {
+        PermissionUtils.PermissionDeniedDialog
+                .newInstance(true).show(getSupportFragmentManager(), "dialog");
+    }
 
 }
